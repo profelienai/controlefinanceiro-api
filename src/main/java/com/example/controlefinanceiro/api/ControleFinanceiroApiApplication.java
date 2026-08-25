1eb2a0f5bd88d1d70cb06d0893d3a82accf43f43
satri <satri@DESKTOP-HATH6ID>
2026-08-25 20:30:25 -0300

    Cria projeto da API

---
+++ src/main/java/com/example/controlefinanceiro/api/ControleFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlefinanceiro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroApiApplication.class, args);
+	}
+
+}
