0a1872760906234bfc81f9c80cfe768723a9ac53
Elienai <profelienai.vr@gmail.com>
2026-08-31 19:12:19 -0300

    feat(validation): adicionar validacao com Bean Validation
    - adiciona validacoes ao recurso de categorias
    - configura mensagens de validacao
    - trata valores invalidos nas requisicoes

--- src/main/java/com/example/controlefinanceiro/api/exceptionHandler/ControleFinanceiroExceptionHandler.java
+++ src/main/java/com/example/controlefinanceiro/api/exceptionHandler/ControleFinanceiroExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.controlefinanceiro.api.exceptionHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,6 +10,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -28,6 +34,26 @@ public class ControleFinanceiroExceptionHandler extends ResponseEntityExceptionH
 		String mensagemDesenvolvedor = ex.getCause().toString();
 		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
 	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
+		List<Erro> erros = new ArrayList<>();
+		
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		}
+			
+		return erros;
+	}	
 	
 	@Getter
 	@AllArgsConstructor
