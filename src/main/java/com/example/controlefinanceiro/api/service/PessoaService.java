6b7d9e3b1bbf689ecf2f6a25c254ad5a644e5a76
Elienai <profelienai.vr@gmail.com>
2026-09-01 19:47:29 -0300

    Implementa atualização de pessoa com PUT

---
+++ src/main/java/com/example/controlefinanceiro/api/service/PessoaService.java
@@ -0,0 +1,24 @@
+package com.example.controlefinanceiro.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.controlefinanceiro.api.model.Pessoa;
+import com.example.controlefinanceiro.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+ 	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.findById(codigo)
+                                .orElseThrow(() -> new EmptyResultDataAccessException(1));
+		
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}   
+}
