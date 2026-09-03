5ac95f4d2b9ab83d45dea676f4b1a01dedc0ba29
Elienai <profelienai.vr@gmail.com>
2026-09-03 19:48:21 -0300

    Valida recurso de lançamento com Bean Validation

--- src/main/java/com/example/controlefinanceiro/api/model/Lancamento.java
+++ src/main/java/com/example/controlefinanceiro/api/model/Lancamento.java
@@ -13,6 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -29,8 +31,10 @@ public class Lancamento {
 	@EqualsAndHashCode.Include
 	private Long codigo;
 	
+	@NotBlank
 	private String descricao;
 
+	@NotNull
 	@Column(name = "data_vencimento")
 	//@JsonFormat(pattern =  "dd/MM/yyyy")
 	private LocalDate dataVencimento;
@@ -39,17 +43,21 @@ public class Lancamento {
 	//@JsonFormat(pattern =  "dd/MM/yyyy")
 	private LocalDate dataPagamento;
 
+	@NotNull
 	private BigDecimal valor;
 
 	private String observacao;
 
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private TipoLancamento tipo;
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
 	private Categoria categoria;
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;
