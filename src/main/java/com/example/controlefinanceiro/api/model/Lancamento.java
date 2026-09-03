5b2a1627e29d145c66e2794b0a7279c62454fe45
Elienai <profelienai.vr@gmail.com>
2026-09-03 19:24:29 -0300

    Implementa recurso de lançamento

---
+++ src/main/java/com/example/controlefinanceiro/api/model/Lancamento.java
@@ -0,0 +1,56 @@
+package com.example.controlefinanceiro.api.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "lancamento")
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Lancamento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long codigo;
+	
+	private String descricao;
+
+	@Column(name = "data_vencimento")
+	//@JsonFormat(pattern =  "dd/MM/yyyy")
+	private LocalDate dataVencimento;
+
+	@Column(name = "data_pagamento")
+	//@JsonFormat(pattern =  "dd/MM/yyyy")
+	private LocalDate dataPagamento;
+
+	private BigDecimal valor;
+
+	private String observacao;
+
+	@Enumerated(EnumType.STRING)
+	private TipoLancamento tipo;
+
+	@ManyToOne
+	@JoinColumn(name = "codigo_categoria")
+	private Categoria categoria;
+
+	@ManyToOne
+	@JoinColumn(name = "codigo_pessoa")
+	private Pessoa pessoa;
+}
